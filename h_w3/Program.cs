ec4f5a700263a1b687d2b91f377eb11ebb97e1f3
SergeiShilkoysky <sergshrb79@gmail.com>
2023-02-19 07:58:26 +0300

    таблица кубов числел от 1 до N

--- h_w3/Program.cs
+++ h_w3/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 23. Напишите программу, которая принимает на вход число (N) и 
+//выдаёт таблицу кубов чисел от 1 до N. / 3 -> 1, 8, 27  / 5 -> 1, 8, 27, 64, 125
+
+void WritePowTableN (int num)
+{
+    for (int i = 1; i <= num; i++)
+    {
+        Console.Write($"{Math.Pow(i, 3)}");
+        Console.Write(i == num ? "." : ", ");
+    }
+    Console.WriteLine("");
+}
+WritePowTableN(3);
+WritePowTableN(5);
