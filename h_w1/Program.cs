dfa4c673372082eb45e9f9a025614d491ff67608
SergeiShilkoysky <sergshrb79@gmail.com>
2023-02-21 14:57:00 +0300

    ДЗ №21_ расстояние м-ду точками в 3D простр.

--- h_w1/Program.cs
+++ h_w1/Program.cs
@@ -1,5 +1,63 @@
 ﻿// Задача 19.  Напишите программу, которая принимает на вход пятизначное число и проверяет,
 // является ли оно палиндромом.  / 14212 -> нет / 12821 -> да / 23432 -> да
 
-Console.WriteLine("Введите пятичначное число: ");
-int Chislo = Convert.ToInt32(Console.ReadLine());
+
+//int chislo = Convert.ToInt32(Console.ReadLine());
+//int chislo = new Random().Next(10000, 100000);
+//Console.WriteLine($"трехзначное число {chislo}");
+
+
+/*
+void CheckPolindrome ();
+{
+    Console.WriteLine("Введите пятичначное число: ");
+int array [] = Convert.ToInt32(Console.ReadLine());
+int count = array.Length;
+{
+   // for (int i = 0; i < count; i++) // проходим по всем элементам массива
+    //{
+    //    Console.Write($"{array[i]} "); // показываем на экране в строку чз пробел
+    //}
+    //Console.WriteLine();
+}
+
+
+
+    //Console.WriteLine($"число {} - полиндром");
+//else
+    //Console.WriteLine($"число {} не является полиндромом");
+
+
+
+/*void PrintPolindr
+ {
+    int count = array.Length; // получение к-ва элементов массива
+    for (int i = 0; i < count; i++) // проходим по всем элементам массива
+    {
+        Console.Write($"{array[i]} "); // показываем на экране в строку чз пробел
+    }
+    Console.WriteLine();
+}
+
+
+//PrintPolindr()
+*/
+
+
+Console.Clear();
+Console.Write("Введите число: ");
+string array = Console.ReadLine();
+int d = 0;
+for ( int i = 0; i < array.length(); i++)
+{
+    if (array[i] == array.length() - 1 - i)
+       {
+        d++;
+       }
+}
+if (d == array.length())
+{
+    Console.WriteLine($"это полиндром");
+}
+else
+    Console.WriteLine($"{array} - не является полиндромом");
