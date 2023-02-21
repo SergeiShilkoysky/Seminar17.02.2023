195f05eb05958cdebb9197b601756b59031a3ac6
SergeiShilkoysky <sergshrb79@gmail.com>
2023-02-21 17:20:35 +0300

    проверка на полиндром

--- h_w1/Program.cs
+++ h_w1/Program.cs
@@ -2,48 +2,6 @@
 // является ли оно палиндромом.  / 14212 -> нет / 12821 -> да / 23432 -> да
 
 
-//int chislo = Convert.ToInt32(Console.ReadLine());
-//int chislo = new Random().Next(10000, 100000);
-//Console.WriteLine($"трехзначное число {chislo}");
-
-
-/*
-void CheckPolindrome ();
-{
-    Console.WriteLine("Введите пятичначное число: ");
-int array [] = Convert.ToInt32(Console.ReadLine());
-int count = array.Length;
-{
-   // for (int i = 0; i < count; i++) // проходим по всем элементам массива
-    //{
-    //    Console.Write($"{array[i]} "); // показываем на экране в строку чз пробел
-    //}
-    //Console.WriteLine();
-}
-
-
-
-    //Console.WriteLine($"число {} - полиндром");
-//else
-    //Console.WriteLine($"число {} не является полиндромом");
-
-
-
-/*void PrintPolindr
- {
-    int count = array.Length; // получение к-ва элементов массива
-    for (int i = 0; i < count; i++) // проходим по всем элементам массива
-    {
-        Console.Write($"{array[i]} "); // показываем на экране в строку чз пробел
-    }
-    Console.WriteLine();
-}
-
-
-//PrintPolindr()
-*/
-
-
 Console.Clear();
 Console.Write("для проверки на ПОЛИНДРОМНОСТЬ введите ЧИСЛОВУЮ либо БУКВЕННУЮ последовательность: ");
 string array = Console.ReadLine();
